3b008661e4e15d5f24c393640976bf38239c41be
unknown <f20170855@hyderabad.bits-pilani.ac.in>
2021-05-03 02:02:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const app = express();
+const items = ["eat", "code", "sleep", "repeat"];
+const workItems = [];
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  const day = date.getDate();
+  res.render("list", { ListTitle: day, newlistItems: items });
+});
+
+app.post("/", function (req, res) {
+  if (req.body.list === "work") {
+    workItems.push(req.body.newitem);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newitem);
+    res.redirect("/");
+  }
+});
+app.get("/work", function (req, res) {
+  res.render("list", { ListTitle: "work", newlistItems: workItems });
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server running on port 3000");
+});
